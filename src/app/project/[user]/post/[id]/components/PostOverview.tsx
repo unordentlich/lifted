e7fbda78fba796eccce1fdc2f7e811978e0acd20
Jonas <hello@jonas.coffee>
2025-01-15 11:31:45 +0100

    Added reply bar

--- src/app/project/[user]/post/[id]/components/PostOverview.tsx
+++ src/app/project/[user]/post/[id]/components/PostOverview.tsx
@@ -1,11 +1,21 @@
 import InlinePost from '@/app/project/home/components/inlinepost/InlinePost';
 import styles from './Post.module.css';
 import { Post } from '@/types/Post';
+import { GrChat, GrSend } from 'react-icons/gr';
 
 export default function PostOverview({ originPost }: { originPost: Post }) {
     return (
         <div>
-            <InlinePost post={originPost} className={styles.originPostCard} linkDeactivated={true}  />
+            <div>
+                <InlinePost post={originPost} className={styles.originPostCard} linkDeactivated={true} />
+                <div className={styles.replyBar}>
+                    <GrChat className={styles.replyIcon}  />
+                    <input type="text" placeholder="Reply to this post" />
+                    <button>
+                        <GrSend/>
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
