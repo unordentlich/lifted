755585a631122c360dacc35770dc94f594a14d82
Jonas <hello@jonas.coffee>
2025-01-24 01:52:24 +0100

    Animated scrollbar

--- styles/components/searchbar/Searchbar.tsx
+++ styles/components/searchbar/Searchbar.tsx
@@ -1,14 +1,19 @@
+'use client';
+import { useRef, useState } from "react";
 import styles from "./Searchbar.module.css";
 import { GrFormSearch } from "react-icons/gr";
 
 export default function Searchbar() {
+    const [placeholderOpacity, setPlaceholderOpacity] = useState(1);
+    const searchElement = useRef<HTMLInputElement>(null);
+    
     return (
         <div className={styles.searchbarContainer}>
-            <div className={styles.placeholder}>
+            <div className={styles.placeholder} style={{opacity: placeholderOpacity}}>
                 <GrFormSearch className={styles.searchbarIcon} />
                 <p>Search</p>
             </div>
-            <input type="text" />
+            <input type="text" ref={searchElement} onFocus={() => {setPlaceholderOpacity(0)}} onBlur={() => {if(searchElement.current?.value.length === 0) setPlaceholderOpacity(1)}}/>
         </div>
     );
 }
